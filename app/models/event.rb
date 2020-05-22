d5e4595f29dfafd606af39bc61310a27ed0ef64b
Michael Kogan <mike@sliday.com>
2020-05-22 15:42:36 -0500

    added user-event reference and validations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,8 @@
 class Event < ApplicationRecord
+  belongs_to :user
+
+  validates :user, presence: true
+
   validates :title, presence: true, length: { maximum: 255 }
   validates :description, presence: true
   validates :address, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+  has_many :events
+
+  before_save { email.downcase! }
+
+  validates :name, presence: true, length: { maximum: 40 }
+
+  validates :email, presence: true,
+            email: true,
+            uniqueness: { case_sensitive: false }
 end
