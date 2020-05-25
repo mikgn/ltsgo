6d2636e2d1cde8cb8e2049ed150dd0f447d97477
Michael Kogan <mike@sliday.com>
2020-05-25 16:01:15 -0500

    refactored views and implemented datetimepicker

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,14 +3,27 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 import "bootstrap"
-require("@rails/ujs").start();
-require("turbolinks").start();
-require("@rails/activestorage").start();
-require("channels");
+import "@rails/ujs"
+import "turbolinks"
+import "@rails/activestorage"
+import "channels"
+import "jquery"
+import "flatpickr"
+import flatpickr from "flatpickr";
 
 $(function () {
     $('[data-toggle="tooltip"]').tooltip()
 });
+
+$(document).ready(function(){
+    flatpickr('#flatpickr', {
+        enableTime: true,
+        minDate: "today",
+        dateFormat: "j/n/Y H:i",
+        allowInput: false
+    });
+});
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
