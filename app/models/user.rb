3e2f89c60dd0362506bb0ae686f4e1214ed08dcc
Michael Kogan <mike@sliday.com>
2020-06-08 12:07:43 -0500

    added subscription assignment after registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,11 +9,17 @@ class User < ApplicationRecord
 
   before_validation :set_name, on: :create
 
+  after_commit :link_subscriptions, on: :create
+
   validates :name, presence: true, length: { maximum: 35 }
 
   private
 
   def set_name
     self.name = "#{I18n.t('activerecord.models.user')}#{rand(100000)}" if self.name.blank?
+  end
+
+  def link_subscriptions
+    Subscription.where(user_id: nil, user_email: self.email).update_all(user_id: self.id)
   end
 end
