95488819807bc54d9eca8a24466e15a2d78db523
Michael Kogan <mike@sliday.com>
2020-06-09 13:26:34 -0500

    added validation for self event subscription

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -2,6 +2,8 @@ class Subscription < ApplicationRecord
   belongs_to :user, optional: true
   belongs_to :event
 
+  before_validation :self_event?, on: :create
+
   validates :user_name, presence: true, unless: -> { user.present? }
   validates :user_email, presence: true,
                          format: Devise.email_regexp,
@@ -16,5 +18,13 @@ class Subscription < ApplicationRecord
 
   def user_email
     user.present? ? user.email : super
+  end
+
+  private
+
+  def self_event?
+    if self.user_id == self.event.user_id
+      errors.add(:self_event, I18n.t('form.errors.self_event'))
+    end
   end
 end
