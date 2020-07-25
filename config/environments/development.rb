c5b47278ba66a80cf38bd2b70a0d7c14e25eb5c3
Michael Kogan <mike@sliday.com>
2020-07-25 17:59:15 -0500

    added From smtp default option for development and production

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,6 +41,8 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
 
+  config.action_mailer.default_options  = { from: 'LTSGo App' }
+
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.smtp_settings = {
